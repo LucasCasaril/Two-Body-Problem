b7d8c218ff7ed396686d022d5ad400971ac9253d
LucasCasaril <lucascasaril@hotmail.com>
2022-06-27 21:56:23 -0300

    Feature: Writing the Runge-Kutta-Fehlberg Method Using RK4 into RK5

--- rkf_method.py
+++ rkf_method.py
@@ -2,21 +2,9 @@
 Runge-Kutta-Fehlberg's Method - Using a Variable Step Size embedding of RK4 into RK5
 
 """
-import numpy as np
-from motion import dydt
 
-#def rkf_integration(dydt, t0, tf, y0, G, m1, m2, tol):
-
-# Initial Parameters
-t0 = 0
-tf = 100
-y0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-G = 6.67259e-20
-m1 = 1e26
-m2 = 1e26
-tol = 1e-8
-
-# Accorting to Fehlberg, the coefficients are:
+'''
+Accorting to Fehlberg (1969), the coefficients are:
 
 a = [0, 1/4, 3/8, 12/13, 1, 1/2]
 
@@ -30,57 +18,81 @@ b = [[    0,          0,          0,         0,         0 ],
 c4 = [25/216,  0,  1408/2565,    2197/4104,   -1/5,    0  ] # For RK4
 
 c5 = [16/135,  0,  6656/12825,  28561/56430,  -9/50,  2/55] # For RK5
+'''
 
-# Initial Conditions
+'''
+NEED FIXING:
+f - Não estamos avaliando f1 a f6
+'''
 
-t = t0
-y = y0
+import numpy as np
+from motion import dydt
 
-# We need to assume the first time step
+def rkf_integration(dydt, t0, tf, y0, G, m1, m2, tol):
 
-h = (tf - t0)/100
+     # Initial Parameters
+     t0 = 0
+     tf = 100
+     y0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+     G = 6.67259e-20
+     m1 = 1e26
+     m2 = 1e26
+     tol = 1e-8
 
-# Os vetores serão variaveis - Usaremos listas e depois iremos transformar
-# em vetores
+     # Initial Conditions
 
-while t < tf:
+     t = t0
+     y = y0
 
-     t_i = t
-     y_i = y
+     y_result = []
+     y_result.append(y0) # The first row of the result is going to be the Initial Condition
 
-     for i in range(5):
-          t_inner = t_i + a[i]*h
-          y_inner = y_i
+     # We need to assume the first time step
 
-          for j in range(i-1):
-               y_inner = y_inner + h*b[i][j]*f[:,j]
+     h = (tf - t0)/100
 
-          # Calling the Differential Equation of the Problem
-          f = dydt(t_i, y_i, G, m1, m2)
+     while t < tf:
 
-     # Truncation Error
+          h_old = h
 
-     te = h * f * [np.subtract(c4, c5)]
-     te_abs = [abs(ele) for ele in te]
-     te_max = max(te_abs)
+          # dydt(t,y_i, G, m1, m2)
 
-     # Allowed Truncation Error
+          k1 = h * dydt(t, y, G, m1, m2)
+          k2 = h * dydt(t + h/4, y + k1/4)
+          k3 = h * dydt(t + 3*h/8, y + 3*k1/32 + 9*k2/32)
+          k4 = h * dydt(t + 12*h/13, y + 1932*k1/2197 - 7200*k2/2197 + 7296*k3/2197)
+          k5 = h * dydt(t + h, y + 439*k1/216 - 8*k2 + 3680*k3/513 - 845*k4/4104)
+          k6 = h * dydt(t + h/2, y - 8*k1/27 + 2*k2 - 3544*k3/2565 + 1859*k4/4104 - 11*k5/40)
 
-     y_abs = [abs(ele) for ele in y]
-     y_max =S max(y_abs)
+          y_order4 = y + 25*k1/216 + 1408*k3/2565 + 2197*k4/4104 - k5/5
+          y_order5 = y + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 - 9*k5/50 + 2*k6/55
 
-     te_allowed = tol * max(y_max,1)
+          # Putting the new iteration in a row
+          y_result.append(y_order5) # Better value for the solution - Runge-Kutta' Method Order 5
 
-     # Change in Step Size
+          e_vector = r_sub = np.subtract(y_order5, y_order4)
 
-     h_delta = (te_allowed/(te_max))^(1/5)
+          # The Truncation Error is the largest of the Abs values of the vector
 
-     # Truncation Error is in bounds
+          e_abs = [abs(ele) for ele in e_vector]
+          e_max = max(e_abs) # Max value of the truncation error
 
-     if te_max <= te_allowed:
+          # The Truncation Error (e_max) cannot exceed the tolerance (tol). We can adjust the step size
+          # so as to keep the error from exceeding the tolerance
+     
+          # Using Gurevich, Svetlana (2017) to calculate the iteration of the step size
+          if e_max >= tol:
+               # h_new < h_old
 
-          h = min(h, (tf-t))
-          t = t + h
-          y = y_i + h * f * c5
+               beta = 0.8
+               h = h_old * beta * pow((tol/e_max),0.2)
+     
 
-          y_result = # input line in each iteration
+          if e_max < tol:
+               # h_new > h_old
+
+               beta = 0.8
+               h = h_old * beta * pow((tol/e_max),0.25)
+
+
+     return y_result
